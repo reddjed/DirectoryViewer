b67c9a39a9b3049235d20e0177c4308d07cab989
Reddjed <rudnicki.kirill@gmail.com>
2022-06-17 15:32:00 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(63, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.treeView1.Location = new System.Drawing.Point(12, 62);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(115, 507);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Location = new System.Drawing.Point(133, 62);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(512, 507);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.SmallIcon;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 21);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "<-";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 21);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "->";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(143, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(502, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 581);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.comboBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(310, 350);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,566 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        ImageList imageList = new ImageList();
+
+        UndoRedoStack<string> filePath = new UndoRedoStack<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.Items.AddRange(Environment.GetLogicalDrives());
+            comboBox1.SelectedIndex = 0;
+
+            System.Drawing.Image image = Image.FromFile(@"ico\fold.jpg");
+
+            imageList.Images.Add("fold", image);
+
+            listView1.LargeImageList = imageList;
+
+            button1.Enabled = false;
+            button2.Enabled = false;
+
+            //TreeNode root = new TreeNode(comboBox1.SelectedItem.ToString(), 0, 0);
+            //root.Nodes.Add(new TreeNode());
+            //treeView1.Nodes.Add(root);
+
+            treeView1.BeforeExpand += TreeView1_BeforeExpand;
+            treeView1.AfterExpand += TreeView1_AfterExpand;
+            treeView1.NodeMouseDoubleClick += TreeView1_NodeMouseDoubleClick;
+
+            //FileSystemWatcher watcher = new FileSystemWatcher();
+            //watcher.Created += Watcher_Created;
+
+            //watcher.Path = @"C:\Users\us\source\repos\TreeView";
+            //watcher.EnableRaisingEvents = true;
+        }
+
+        private void TreeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            Process.Start(e.Node.FullPath);
+            //throw new NotImplementedException();
+        }
+
+        //private void Watcher_Created(object sender, FileSystemEventArgs e)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        private void TreeView1_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void TreeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+
+            e.Node.Nodes.Clear();
+            //string[] files = Directory.GetFiles(e.Node.FullPath);
+            try
+            {
+                string[] dirs = Directory.GetDirectories(e.Node.FullPath);
+                label1.Text = e.Node.FullPath;
+                foreach (var d in dirs)
+                {
+                    DirectoryInfo di = new DirectoryInfo(d);
+                    TreeNode node = new TreeNode(di.Name, 2, 2);
+
+                    node.ImageKey = "folder";
+                    node.Tag = di;
+                    node.Nodes.Add(new TreeNode());
+
+                    e.Node.Nodes.Add(node);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.Source);
+                return;
+            }
+            //foreach (var f in files)
+            //{
+            //    FileInfo fi = new FileInfo(f);
+            //    TreeNode node = new TreeNode(fi.Name, 2, 2);
+
+            //    e.Node.Nodes.Add(node);
+            //}
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            imageList.Images.RemoveByKey(".exe");
+            listView1.Clear();
+            ListViewItem.ListViewSubItem[] subItems;
+            ListViewItem item = null;
+
+            filePath.Clear();
+            button1.Enabled = false;
+            button2.Enabled = false;
+
+            treeView1.Nodes.Clear();
+
+            TreeNode root = new TreeNode(comboBox1.SelectedItem.ToString(), 0, 0);
+            root.Nodes.Add(new TreeNode());
+            treeView1.Nodes.Add(root);
+
+            label1.Text = comboBox1.SelectedItem.ToString();
+            string[] dirs = { };
+            string[] files = { };
+            try
+            {
+                dirs = Directory.GetDirectories(comboBox1.SelectedItem.ToString());
+                files = Directory.GetFiles(comboBox1.SelectedItem.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.Source);
+            }
+
+            filePath.Push(comboBox1.SelectedItem.ToString());
+
+            foreach (var f in files)
+            {
+                FileInfo fi = new FileInfo(f);
+
+                if (!imageList.Images.ContainsKey(fi.Extension))
+                {
+                    Icon iconForFile = System.Drawing.Icon.ExtractAssociatedIcon(fi.FullName);
+                    imageList.Images.Add(fi.Extension, iconForFile);
+                }
+
+                item = new ListViewItem(fi.Name);
+                subItems = new ListViewItem.ListViewSubItem[]
+                {
+                    new ListViewItem.ListViewSubItem(item, fi.FullName),
+                    new ListViewItem.ListViewSubItem(item, fi.LastAccessTime.ToShortDateString())
+                };
+
+                item.ImageKey = fi.Extension;
+
+                item.SubItems.AddRange(subItems);
+                listView1.Items.Add(item);
+            }
+
+            foreach (var d in dirs)
+            {
+                DirectoryInfo dir = new DirectoryInfo(d);
+                item = new ListViewItem(dir.Name);
+
+                item.ImageIndex = 0;
+                subItems = new ListViewItem.ListViewSubItem[]
+                {
+                    new ListViewItem.ListViewSubItem(item, dir.FullName),
+                    new ListViewItem.ListViewSubItem(item, dir.LastAccessTime.ToShortDateString())
+                };
+
+                item.ImageKey = "fold";
+                item.SubItems.AddRange(subItems);
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            imageList.Images.RemoveByKey(".exe");
+
+            TreeNode newSelected = e.Node;
+            listView1.Items.Clear();
+            DirectoryInfo nodeDirInfo = (DirectoryInfo)newSelected.Tag;
+
+            ListViewItem.ListViewSubItem[] subItems;
+            ListViewItem item = null;
+
+            label1.Text = e.Node.FullPath;
+
+            if (newSelected.Level == 0)
+            {
+                string[] dirs = Directory.GetDirectories(e.Node.FullPath);
+                string[] files = Directory.GetFiles(e.Node.FullPath);
+
+                foreach (var f in files)
+                {
+                    FileInfo fi = new FileInfo(f);
+
+                    if (!imageList.Images.ContainsKey(fi.Extension))
+                    {
+                        Icon iconForFile = System.Drawing.Icon.ExtractAssociatedIcon(fi.FullName);
+                        imageList.Images.Add(fi.Extension, iconForFile);
+                    }
+
+                    item = new ListViewItem(fi.Name);
+                    subItems = new ListViewItem.ListViewSubItem[]
+                    {
+                        new ListViewItem.ListViewSubItem(item, fi.FullName),
+                        new ListViewItem.ListViewSubItem(item, fi.LastAccessTime.ToShortDateString())
+                    };
+
+                    item.ImageKey = fi.Extension;
+
+                    item.SubItems.AddRange(subItems);
+                    listView1.Items.Add(item);
+                }
+
+                foreach (var d in dirs)
+                {
+                    DirectoryInfo dir = new DirectoryInfo(d);
+                    item = new ListViewItem(dir.Name);
+                    item.ImageIndex = imageList.Images.Count - 1;
+                    subItems = new ListViewItem.ListViewSubItem[]
+                    {
+                        new ListViewItem.ListViewSubItem(item, dir.FullName),
+                        new ListViewItem.ListViewSubItem(item, dir.LastAccessTime.ToShortDateString())
+                    };
+
+                    item.ImageKey = "fold";
+
+                    item.SubItems.AddRange(subItems);
+                    listView1.Items.Add(item);
+                }
+            }
+            else
+            {
+                try
+                {
+                    foreach (DirectoryInfo dir in nodeDirInfo.GetDirectories())
+                    {
+
+                        item = new ListViewItem(dir.Name);
+                        item.ImageIndex = imageList.Images.Count - 1;
+                        subItems = new ListViewItem.ListViewSubItem[]
+                        {
+                            new ListViewItem.ListViewSubItem(item, dir.FullName),
+                            new ListViewItem.ListViewSubItem(item, dir.LastAccessTime.ToShortDateString())
+                        };
+
+                        item.ImageKey = "fold";
+
+                        item.SubItems.AddRange(subItems);
+                        listView1.Items.Add(item);
+                    }
+                    foreach (FileInfo file in nodeDirInfo.GetFiles())
+                    {
+
+                        if (!imageList.Images.ContainsKey(file.Extension))
+                        {
+                            Icon iconForFile = System.Drawing.Icon.ExtractAssociatedIcon(file.FullName);
+                            imageList.Images.Add(file.Extension, iconForFile);
+                        }
+
+                        item = new ListViewItem(file.Name, 1);
+                        subItems = new ListViewItem.ListViewSubItem[]
+                        {
+                            new ListViewItem.ListViewSubItem(item, file.FullName),
+                            new ListViewItem.ListViewSubItem(item, file.LastAccessTime.ToShortDateString())
+                        };
+
+                        item.ImageKey = file.Extension;
+
+                        item.SubItems.AddRange(subItems);
+                        listView1.Items.Add(item);
+                    }
+
+                    listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, ex.Source);
+                    return;
+                }
+
+            }
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            FileInfo fileInfo = new FileInfo(listView1.Items[listView1.Items.IndexOf(listView1.FocusedItem)].SubItems[1].Text);
+
+            if (fileInfo.Exists == true)
+            {
+                Process.Start(listView1.Items[listView1.Items.IndexOf(listView1.FocusedItem)].SubItems[1].Text);
+            }
+
+            else
+            {
+                imageList.Images.RemoveByKey(".exe");
+
+                string[] dirs = { };
+                string[] files = { };
+
+                try
+                {
+                    dirs = Directory.GetDirectories(listView1.Items[listView1.Items.IndexOf(listView1.FocusedItem)].SubItems[1].Text);
+                    files = Directory.GetFiles(listView1.Items[listView1.Items.IndexOf(listView1.FocusedItem)].SubItems[1].Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, ex.Source);
+                    return;
+                }
+                filePath.Push(listView1.Items[listView1.Items.IndexOf(listView1.FocusedItem)].SubItems[1].Text);
+
+                button1.Enabled = true;
+
+                label1.Text = listView1.Items[listView1.Items.IndexOf(listView1.FocusedItem)].SubItems[1].Text;
+
+                listView1.Items.Clear();
+                ListViewItem.ListViewSubItem[] subItems;
+                ListViewItem item = null;
+
+                foreach (var f in files)
+                {
+                    FileInfo fi = new FileInfo(f);
+
+                    if (!imageList.Images.ContainsKey(fi.Extension))
+                    {
+                        Icon iconForFile = System.Drawing.Icon.ExtractAssociatedIcon(fi.FullName);
+                        imageList.Images.Add(fi.Extension, iconForFile);
+                    }
+
+                    item = new ListViewItem(fi.Name);
+
+                    subItems = new ListViewItem.ListViewSubItem[]
+                    {
+                    new ListViewItem.ListViewSubItem(item, fi.FullName),
+                    new ListViewItem.ListViewSubItem(item, fi.LastAccessTime.ToShortDateString())
+                    };
+
+                    item.ImageKey = fi.Extension;
+
+                    item.SubItems.AddRange(subItems);
+                    listView1.Items.Add(item);
+                }
+
+                foreach (var d in dirs)
+                {
+                    DirectoryInfo dir = new DirectoryInfo(d);
+                    item = new ListViewItem(dir.Name);
+                    item.ImageIndex = imageList.Images.Count - 1;
+                    subItems = new ListViewItem.ListViewSubItem[]
+                    {
+                    new ListViewItem.ListViewSubItem(item, dir.FullName),
+                    new ListViewItem.ListViewSubItem(item, dir.LastAccessTime.ToShortDateString())
+                    };
+
+                    item.ImageKey = "fold";
+
+                    item.SubItems.AddRange(subItems);
+                    listView1.Items.Add(item);
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            imageList.Images.RemoveByKey(".exe");
+
+            filePath.Undo();
+
+            if (!filePath.UndoEnable)
+            {
+                button1.Enabled = false;
+            }
+
+            button2.Enabled = true;
+
+            string[] dirs = { };
+            string[] files = { };
+
+            dirs = Directory.GetDirectories(filePath.CurrentItem);
+            files = Directory.GetFiles(filePath.CurrentItem);
+
+            label1.Text = filePath.CurrentItem;
+
+            listView1.Items.Clear();
+            ListViewItem.ListViewSubItem[] subItems;
+            ListViewItem item = null;
+
+            foreach (var f in files)
+            {
+                FileInfo fi = new FileInfo(f);
+
+                if (!imageList.Images.ContainsKey(fi.Extension))
+                {
+                    Icon iconForFile = System.Drawing.Icon.ExtractAssociatedIcon(fi.FullName);
+                    imageList.Images.Add(fi.Extension, iconForFile);
+                }
+
+                item = new ListViewItem(fi.Name);
+                subItems = new ListViewItem.ListViewSubItem[]
+                {
+                    new ListViewItem.ListViewSubItem(item, fi.FullName),
+                    new ListViewItem.ListViewSubItem(item, fi.LastAccessTime.ToShortDateString())
+                };
+
+                item.ImageKey = fi.Extension;
+
+                item.SubItems.AddRange(subItems);
+                listView1.Items.Add(item);
+            }
+
+            foreach (var d in dirs)
+            {
+                DirectoryInfo dir = new DirectoryInfo(d);
+                item = new ListViewItem(dir.Name);
+                item.ImageIndex = imageList.Images.Count - 1;
+                subItems = new ListViewItem.ListViewSubItem[]
+                {
+                    new ListViewItem.ListViewSubItem(item, dir.FullName),
+                    new ListViewItem.ListViewSubItem(item, dir.LastAccessTime.ToShortDateString())
+                };
+
+                item.ImageKey = "fold";
+
+                item.SubItems.AddRange(subItems);
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            imageList.Images.RemoveByKey(".exe");
+
+            filePath.Redo();
+
+            if (!filePath.RedoEnable)
+            {
+                button2.Enabled = false;
+            }
+
+            button1.Enabled = true;
+
+            string[] dirs = { };
+            string[] files = { };
+
+            dirs = Directory.GetDirectories(filePath.CurrentItem);
+            files = Directory.GetFiles(filePath.CurrentItem);
+
+            label1.Text = filePath.CurrentItem;
+
+            listView1.Items.Clear();
+            ListViewItem.ListViewSubItem[] subItems;
+            ListViewItem item = null;
+
+            foreach (var f in files)
+            {
+                FileInfo fi = new FileInfo(f);
+
+                if (!imageList.Images.ContainsKey(fi.Extension))
+                {
+                    Icon iconForFile = System.Drawing.Icon.ExtractAssociatedIcon(fi.FullName);
+                    imageList.Images.Add(fi.Extension, iconForFile);
+                }
+
+                item = new ListViewItem(fi.Name);
+                subItems = new ListViewItem.ListViewSubItem[]
+                {
+                    new ListViewItem.ListViewSubItem(item, fi.FullName),
+                    new ListViewItem.ListViewSubItem(item, fi.LastAccessTime.ToShortDateString())
+                };
+
+                item.ImageKey = fi.Extension;
+
+                item.SubItems.AddRange(subItems);
+                listView1.Items.Add(item);
+            }
+
+            foreach (var d in dirs)
+            {
+                DirectoryInfo dir = new DirectoryInfo(d);
+                item = new ListViewItem(dir.Name);
+                item.ImageIndex = imageList.Images.Count - 1;
+                subItems = new ListViewItem.ListViewSubItem[]
+                {
+                    new ListViewItem.ListViewSubItem(item, dir.FullName),
+                    new ListViewItem.ListViewSubItem(item, dir.LastAccessTime.ToShortDateString())
+                };
+
+                item.ImageKey = "fold";
+
+                item.SubItems.AddRange(subItems);
+                listView1.Items.Add(item);
+            }
+        }
+
+        public class UndoRedoStack<T>
+        {
+            private Stack<T> undoStack = new Stack<T>();
+            private Stack<T> redoStack = new Stack<T>();
+
+            public T CurrentItem { get; private set; }
+
+            public bool UndoEnable
+            {
+                get { return undoStack.Count > 0; }
+            }
+
+            public bool RedoEnable
+            {
+                get { return redoStack.Count > 0; }
+            }
+
+            public void Push(T item)
+            {
+                if (!CurrentItemIsEmpty)
+                    undoStack.Push(CurrentItem);
+
+                CurrentItem = item;
+                redoStack.Clear();
+            }
+
+            public void Undo()
+            {
+                if (undoStack.Count == 0)
+                    return;
+
+                if (!CurrentItemIsEmpty)
+                    redoStack.Push(CurrentItem);
+
+                CurrentItem = undoStack.Pop();
+            }
+
+            public void Redo()
+            {
+                if (redoStack.Count == 0)
+                {
+                    return;
+                }
+
+
+                if (!CurrentItemIsEmpty)
+                    undoStack.Push(CurrentItem);
+
+                CurrentItem = redoStack.Pop();
+            }
+
+            public void Clear()
+            {
+                CurrentItem = default(T);
+                undoStack.Clear();
+                redoStack.Clear();
+            }
+
+            private bool CurrentItemIsEmpty
+            {
+                get { return object.Equals(CurrentItem, default(T)); }
+            }
+        }
+    }
+}
